e5bcf222c23560007bb7024bd89bf4e2c9eefa8e
🧑🏿‍💻 ElyonCodes <104038966+Lawani-EJ@users.noreply.github.com>
2025-02-24 08:23:05 +0100

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,51 +1,52 @@
-export const bio =
-  "My name is Lawani Elyon John, but you can call me John, I'm a Passionate developer with experience in building fullstack web applications using JavaScript, React.js, Next.js, Node.js, and various other libraries and frameworks.";
+export const bio = 
+  "My name is Lawani Elyon John, but you can call me John. I'm a passionate software developer with experience in building full-stack web applications using JavaScript, React.js, Next.js, Node.js, and a variety of modern libraries and frameworks. I love creating seamless digital experiences and constantly learning new technologies.";
 
 export const skills = [
-  { name: "Frontend", items: ["React", "Next.js", "Tailwind CSS"] },
+  { name: "Frontend", items: ["React", "Next.js", "Tailwind CSS", "Vite"] },
   { name: "Backend", items: ["Node.js", "Express", "Nest.js"] },
   { name: "Database", items: ["MongoDB", "PostgreSQL", "MySQL"] },
   { name: "CMS & Headless CMS", items: ["WordPress", "Strapi.js"] },
+  { name: "Others", items: ["Firebase", "Gradio", "LangChain"] }
 ];
 
 export const projects = [
   {
-    title: "AI Travel Planner",
+    title: "TalkSync - Meeting Assistant",
     description:
-      "A modern travel planner using Next.js 15, TailwindCSS, Prisma, Open AI, and Clerk.",
-    techs: ["Next.js"],
-    link: "https://github.com/saidMounaim/travelplan",
+      "An AI-powered meeting assistant built with Whisper, LangChain, and Gradio, designed for seamless voice-to-text transcription and meeting summaries.",
+    techs: ["Python", "Gradio", "LangChain"],
+    link: "https://github.com/Lawani-EJ/TalkSync",
   },
   {
-    title: "Meetroomly",
+    title: "Todo Web Application",
     description:
-      "MeetRoomly using Next.js 15, Prisma, Tailwind CSS, and Clerk.",
-    techs: ["Next.js"],
-    link: "https://github.com/saidMounaim/meetroomly",
+      "A simple yet powerful task manager built using React, Tailwind CSS, and Node.js.",
+    techs: ["React", "Tailwind CSS", "Node.js"],
+    link: "https://github.com/Lawani-EJ/Todo-WebApplication",
   },
   {
-    title: "Home Service",
+    title: "Simple Photo Web Application",
     description:
-      "Home Service Full-stack app using NestJs, Next.js, React Query TailwindCSS & Prisma",
-    techs: ["Next.js", "Nest.js"],
-    link: "https://github.com/saidMounaim/home-service",
+      "A photo-sharing app designed for simplicity and ease of use, built using Next.js and Firebase.",
+    techs: ["Next.js", "Firebase"],
+    link: "https://github.com/Lawani-EJ/Simple-Photo-WebApplication",
   },
   {
-    title: "Club Marathon",
+    title: "15ProLandingPage",
     description:
-      "Club Marathon Landing Page using HTML5, CSS3, JavaScript, and GSAP",
-    techs: ["HTML5", "CSS3", "GSAP"],
-    link: "https://clubmarathon.fr/",
+      "A landing page for showcasing the features of the iPhone 15 Pro using React and Tailwind CSS.",
+    techs: ["React", "Tailwind CSS"],
+    link: "https://github.com/Lawani-EJ/15ProLandingPage",
   },
 ];
 
 export const infos = {
   fullName: "Lawani Elyon John",
   status: "Full Stack JavaScript Developer",
-  shortBio: "Passionate about building scalable web applications.",
+  shortBio: "I build scalable web applications and love working on innovative projects.",
   avatar: "/avatar.jpeg",
   mail: "lawanijohn70@gmail.com",
-  linkedin: "",
-  x: "https://x.com/said_mounaim",
-  github: "https://github.com/saidMounaim",
+  linkedin: "https://www.linkedin.com/in/elyon-john",
+  x: "https://x.com/Lawani_EJ",
+  github: "https://github.com/Lawani-EJ",
 };
